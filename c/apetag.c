5c3bb98363ffd7f65d07d7d2da99deb8ccdf82d5
Jeremy Evans <code@jeremyevans.net>
2012-07-09 12:52:52 -0700

    Add local version of strncasecmp
    On certain platforms, strncasecmp may depend on the user's locale.
    However, the calling code expects a pure-ASCII comparison.  Use
    OpenBSD's strncasecmp code which uses a simple mapping table for
    values in the ASCII range.

--- c/apetag.c
+++ c/apetag.c
@@ -68,6 +68,41 @@ static DB *ID3_GENRES = NULL;
 static uint32_t APE_MAXIMUM_TAG_SIZE = 8192;
 static uint32_t APE_MAXIMUM_ITEM_COUNT = 64;
 
+static const unsigned char charmap[] = {
+    '\000', '\001', '\002', '\003', '\004', '\005', '\006', '\007',
+    '\010', '\011', '\012', '\013', '\014', '\015', '\016', '\017',
+    '\020', '\021', '\022', '\023', '\024', '\025', '\026', '\027',
+    '\030', '\031', '\032', '\033', '\034', '\035', '\036', '\037',
+    '\040', '\041', '\042', '\043', '\044', '\045', '\046', '\047',
+    '\050', '\051', '\052', '\053', '\054', '\055', '\056', '\057',
+    '\060', '\061', '\062', '\063', '\064', '\065', '\066', '\067',
+    '\070', '\071', '\072', '\073', '\074', '\075', '\076', '\077',
+    '\100', '\141', '\142', '\143', '\144', '\145', '\146', '\147',
+    '\150', '\151', '\152', '\153', '\154', '\155', '\156', '\157',
+    '\160', '\161', '\162', '\163', '\164', '\165', '\166', '\167',
+    '\170', '\171', '\172', '\133', '\134', '\135', '\136', '\137',
+    '\140', '\141', '\142', '\143', '\144', '\145', '\146', '\147',
+    '\150', '\151', '\152', '\153', '\154', '\155', '\156', '\157',
+    '\160', '\161', '\162', '\163', '\164', '\165', '\166', '\167',
+    '\170', '\171', '\172', '\173', '\174', '\175', '\176', '\177',
+    '\200', '\201', '\202', '\203', '\204', '\205', '\206', '\207',
+    '\210', '\211', '\212', '\213', '\214', '\215', '\216', '\217',
+    '\220', '\221', '\222', '\223', '\224', '\225', '\226', '\227',
+    '\230', '\231', '\232', '\233', '\234', '\235', '\236', '\237',
+    '\240', '\241', '\242', '\243', '\244', '\245', '\246', '\247',
+    '\250', '\251', '\252', '\253', '\254', '\255', '\256', '\257',
+    '\260', '\261', '\262', '\263', '\264', '\265', '\266', '\267',
+    '\270', '\271', '\272', '\273', '\274', '\275', '\276', '\277',
+    '\300', '\301', '\302', '\303', '\304', '\305', '\306', '\307',
+    '\310', '\311', '\312', '\313', '\314', '\315', '\316', '\317',
+    '\320', '\321', '\322', '\323', '\324', '\325', '\326', '\327',
+    '\330', '\331', '\332', '\333', '\334', '\335', '\336', '\337',
+    '\340', '\341', '\342', '\343', '\344', '\345', '\346', '\347',
+    '\350', '\351', '\352', '\353', '\354', '\355', '\356', '\357',
+    '\360', '\361', '\362', '\363', '\364', '\365', '\366', '\367',
+    '\370', '\371', '\372', '\373', '\374', '\375', '\376', '\377',
+};
+
 /* Private Structure */
 
 struct ApeTag {
@@ -108,6 +143,7 @@ static int ApeTag__check_valid_utf8(unsigned char *utf8_string, uint32_t size);
 static int ApeItem__compare(const void *a, const void *b);
 static int ApeTag__lookup_genre(struct ApeTag *tag, struct ApeItem *item, unsigned char *genre_id);
 static int ApeTag__load_ID3_GENRES(struct ApeTag *tag);
+static int ApeTag__strncasecmp(const char *s1, const char *s2, size_t n);
 
 /* Public Functions */
 
@@ -1184,10 +1220,10 @@ static int ApeItem__check_validity(struct ApeTag *tag, struct ApeItem *item) {
         tag->error = "invalid item key (too long)";
         return -3;
     }
-    if(key_length == 3 ? strncasecmp(item->key, "id3", 3) == 0 || 
-                         strncasecmp(item->key, "tag", 3) == 0 || 
-                         strncasecmp(item->key, "mp+", 3) == 0
-       : (key_length == 4 ? strncasecmp(item->key, "oggs", 4) == 0 : 0)) {
+    if(key_length == 3 ? ApeTag__strncasecmp(item->key, "id3", 3) == 0 || 
+                         ApeTag__strncasecmp(item->key, "tag", 3) == 0 || 
+                         ApeTag__strncasecmp(item->key, "mp+", 3) == 0
+       : (key_length == 4 ? ApeTag__strncasecmp(item->key, "oggs", 4) == 0 : 0)) {
         tag->error = "invalid item key (id3|tag|mp+|oggs)";
         return -3;
     }
@@ -1619,5 +1655,26 @@ static struct ApeItem ** ApeTag__get_items(struct ApeTag *tag, uint32_t *num_ite
     }
 
     return is;
+}
+
+/* 
+Local ASCII-only version of strncasecmp, since default strncasecmp may
+depend on the locale, and this version is only called with APE item keys
+(which are limited to ASCII).
+*/
+static int ApeTag__strncasecmp(const char *s1, const char *s2, size_t n) {
+        if (n != 0) {
+                const unsigned char *cm = charmap;
+                const unsigned char *us1 = (const unsigned char *)s1;
+                const unsigned char *us2 = (const unsigned char *)s2;
+
+                do {
+                        if (cm[*us1] != cm[*us2++])
+                                return (cm[*us1] - cm[*--us2]);
+                        if (*us1++ == '\0')
+                                break;
+                } while (--n != 0);
+        }
+        return (0);
 }
 
